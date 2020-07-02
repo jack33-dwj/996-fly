18a96db264bd709f9c236593ca160dde56574757
计算机1805-陈凯伦-20184667 <67416544+Karen-CZ@users.noreply.github.com>
2020-07-02 16:12:07 +0800

    词法分析

---
+++ cf/preProcess.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#pragma warning(disable:4996)
+using namespace std;
+extern void scanner(int& syn, char resourceproject[], char token[], int& pProject);
+extern void preresource(char r[], int pProject);
